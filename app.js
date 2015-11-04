cab977b7e6b152d6c9ddf4712363a5d45fa6c175
RRothfeld <raoul.rothfeld+github@gmail.com>
2015-11-04 22:42:51 +0100

    bug fixes (no irsend)

--- app.js
+++ app.js
@@ -1,7 +1,119 @@
+// /**
+//  * app.js
+//  * 
+//  * IRS-Himbeere Application
+//  * 
+//  * based on:
+//  * @author Michael Vartan
+//  * @version 1.0.0 
+//  */
+
+// var express = require('express');
+// var app = express();
+// var sys = require('sys');
+// var fs = require('fs');
+// var bodyParser = require('body-parser');
+// var exec = require('child_process').exec;
+
+// /**
+//  * Dictionary of devices and their buttons
+//  * @type {Object}
+//  */
+// var devices = {};
+// /**
+//  * Generates function to get devices' buttons from irsend command
+//  * @param  {String} deviceName name of device
+//  * @return {Function}            exec callback
+//  */ 
+// var getCommandsForDevice = function(deviceName) {
+//   /**
+//    * Get Device's Button from irsend command
+//    * @param  {String} error  Error from running command
+//    * @param  {String} stdout std out
+//    * @param  {String} stderr std err
+//    * @return {None}        
+//    */
+//   return function(error, stdout, stderr) {
+//     var lines = stderr.split("\n");
+//     for(var lineIndex in lines) {
+//       var line = lines[lineIndex];
+//       var parts = line.split(" ");
+//       if(parts.length>2) {
+//         var keyName = parts[2];
+//         devices[deviceName].push(keyName);
+//         console.log(deviceName + " found key: "+keyName);
+//       }
+//     }
+//   }
+// };
+// /**
+//  * Get Device from irsend command
+//  * @param  {String} error  Error from running command
+//  * @param  {String} stdout std out
+//  * @param  {String} stderr std err
+//  * @return {None}        
+//  */
+// var getDevice = function (error, stdout, stderr) {
+//   if(error) {
+//     console.log("irsend not available.");
+//     return;
+//   }
+//   var lines = stderr.split("\n");
+//   for(var lineIndex in lines) {
+//     var line = lines[lineIndex];
+//     var parts = line.split(" ");
+//     if(parts.length>1) {
+//       var deviceName = parts[1];
+//       console.log("device found: "+deviceName.trim());
+//       devices[deviceName] = [];
+//       exec("irsend list \""+deviceName+"\" \"\"", getCommandsForDevice(deviceName));
+
+//     }
+//   }          
+// };
+// // Get all device information
+// exec("irsend list \"\" \"\"", getDevice);
+
+// // Define static HTML files
+// app.use(express.static(__dirname + '/html'));
+
+// // Enable JSON-encoded bodies
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// // Define GET request for /send/deviceName/buttonName
+// app.get('/send/:device/:key', function(req, res) {
+//   var deviceName = req.params.device;
+//   var key = req.params.key.toUpperCase();
+
+//   // send command to irsend
+//   var command = "irsend SEND_ONCE "+deviceName+" "+key;
+//   exec(command, function(error, stdout, stderr){
+//     if(error)
+//       return console.log(error);
+//     else   
+//       res.send("Successfully sent command");
+//   });
+// });
+
+// // Define POST request for /save
+// app.post('/save', function (req, res) {
+//   fs.writeFile(__dirname+"/html/favs.txt", req.body.text, function(err) {
+//     if(err) {
+//       return console.log(err);
+//     } else {
+//       res.send("Successfully saved changes.");
+//     }
+//   });
+// });
+
+// // Listen on port 3000
+// app.listen('3000');
+
 /**
  * app.js
  * 
- * IRS-Himbeere Application
+ * IR Remote Web Service
  * 
  * based on:
  * @author Michael Vartan
@@ -10,22 +122,22 @@
 
 var express = require('express');
 var app = express();
-var sys = require('util');
-var fs = require('fs');
-var bodyParser = require('body-parser');
+var sys = require('sys');
 var exec = require('child_process').exec;
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
 /**
  * Dictionary of devices and their buttons
  * @type {Object}
  */
-var devices = {};
+// var devices = {};
 /**
  * Generates function to get devices' buttons from irsend command
  * @param  {String} deviceName name of device
  * @return {Function}            exec callback
  */ 
-var getCommandsForDevice = function(deviceName) {
+// var getCommandsForDevice = function(deviceName) {
   /**
    * Get Device's Button from irsend command
    * @param  {String} error  Error from running command
@@ -33,64 +145,92 @@ var getCommandsForDevice = function(deviceName) {
    * @param  {String} stderr std err
    * @return {None}        
    */
-  return function(error, stdout, stderr) {
-    var lines = stderr.split("\n");
-    for(var lineIndex in lines) {
-      var line = lines[lineIndex];
-      var parts = line.split(" ");
-      if(parts.length>2) {
-        var keyName = parts[2];
-        devices[deviceName].push(keyName);
-        console.log(deviceName + " found key: "+keyName);
-      }
-    }
-  }
-};
+//   return function(error, stdout, stderr) {
+//     var lines = stderr.split("\n");
+//     for(var lineIndex in lines) {
+//       var line = lines[lineIndex];
+//       var parts = line.split(" ");
+//       if(parts.length>2) {
+//         var keyName = parts[2];
+//         devices[deviceName].push(keyName);
+//         console.log(deviceName + " found key: "+keyName);
+//       }
+//     }
+//   }
+// };
+// /**
+//  * Get Device from irsend command
+//  * @param  {String} error  Error from running command
+//  * @param  {String} stdout std out
+//  * @param  {String} stderr std err
+//  * @return {None}        
+//  */
+// var getDevice = function (error, stdout, stderr) {
+//   if(error) {
+//     console.log("irsend not available.");
+//     return;
+//   }
+//   var lines = stderr.split("\n");
+//   for(var lineIndex in lines) {
+//     var line = lines[lineIndex];
+//     var parts = line.split(" ");
+//     if(parts.length>1) {
+//       var deviceName = parts[1];
+//       console.log("device found: "+deviceName.trim());
+//       devices[deviceName] = [];
+//       exec("irsend list \""+deviceName+"\" \"\"", getCommandsForDevice(deviceName));
+
+//     }
+//   }          
+// };
+// // Get all device information
+// exec("irsend list \"\" \"\"", getDevice);
 
 // Define static HTML files
 app.use(express.static(__dirname + '/html'));
-
-// Enable JSON-encoded bodies
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 // Define GET request for /send/deviceName/buttonName
 app.get('/send/:device/:key', function(req, res) {
   var deviceName = req.params.device;
   var key = req.params.key.toUpperCase();
 
-  // Make sure that the user has requested a valid device 
-  if(!devices.hasOwnProperty(deviceName)) {
-    res.send("invalid device");
-    return;
-  }
+  // // Make sure that the user has requested a valid device 
+  // if(!devices.hasOwnProperty(deviceName)) {
+  //   res.send("invalid device");
+  //   return;
+  // }
 
-  // Make sure that the user has requested a valid key/button
-  var device = devices[deviceName];
-  var deviceKeyFound = false;
-  for(var i = 0; i < device.length; i++) {
-    if(device[i] === key) {
-      deviceKeyFound = true; 
-      break;
-    }
-  }
-  if(!deviceKeyFound) {
-    res.send("invalid key number: "+key);
-    return;
-  }
+  // // Make sure that the user has requested a valid key/button
+  // var device = devices[deviceName];
+  // var deviceKeyFound = false;
+  // for(var i = 0; i < device.length; i++) {
+  //   if(device[i] === key) {
+  //     deviceKeyFound = true; 
+  //     break;
+  //   }
+  // }
+  // if(!deviceKeyFound) {
+  //   res.send("invalid key number: "+key);
+  //   return;
+  // }
 
   // send command to irsend
   var command = "irsend SEND_ONCE "+deviceName+" "+key;
   exec(command, function(error, stdout, stderr){
     if(error)
-      return console.log(error);
+      res.send("Error sending command");
     else   
       res.send("Successfully sent command");
   });
-});
+}); // end define GET request for /send/deviceName/buttonName
+
+// Enable JSON-encoded bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Define POST request for /save
 app.post('/save', function (req, res) {
+  console.log(req.body);
   fs.writeFile(__dirname+"/html/favs.txt", req.body.text, function(err) {
     if(err) {
       return console.log(err);
